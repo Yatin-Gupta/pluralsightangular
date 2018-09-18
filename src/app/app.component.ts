e70724960ffb5245019079564017357e9e50c90c
Yatin Gupta <yatin.gupta@OPTIMUSDOM.COM>
2018-09-18 19:13:08 +0530

    Changes to project added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
 
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <product-list></product-list>
+  `
 })
 export class AppComponent {
   title = 'my-app';
