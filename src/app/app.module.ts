ace79e59dc234766593c6b9412444105b232e298
Yatin Gupta <yatin.gupta@OPTIMUSDOM.COM>
2018-09-19 17:54:23 +0530

    routing implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,9 @@ import {ConvertToSpacesPipe} from './products/shared/convert-to-spaces.pipe';
 import { ProductListFilterPipe } from './products/shared/product-list-filter.pipe';
 import { StarComponent } from './products/shared/star.component';
 import { ProductService } from './products/product.service';
+import { ProductDetailComponent } from './products/product-detail.component';
+import { WelcomeComponent } from './home/welcome.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -16,12 +19,33 @@ import { ProductService } from './products/product.service';
     ProductListComponent,
     ConvertToSpacesPipe,
     ProductListFilterPipe,
-    StarComponent
+    StarComponent,
+    ProductDetailComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      {
+        path: 'products',
+        component: ProductListComponent
+      },
+      {
+        path: 'products/:id',
+        component: ProductDetailComponent
+      },
+      {
+        path: 'welcome',
+        component: WelcomeComponent
+      },
+      {
+        path: '',
+        redirectTo: 'welcome',
+        pathMatch: 'full'
+      }
+    ], {useHash: true})
   ],
   providers: [
     ProductService
